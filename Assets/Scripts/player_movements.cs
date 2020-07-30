908acb091a181f3796aaa61a60474590ff2f59f0
Vinícius Alves Mesquita <vinizmesquita@gmail.com>
2020-07-30 08:17:20 -0300

    Making the Player Movements system

---
+++ Assets/Scripts/player_movements.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movements : MonoBehaviour
+{
+   public float speed;
+   private Rigidbody2D _rb;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody2D>();
+
+    }
+    // Hello Wor
+
+    private void FixedUpdate(){
+        if(Input.GetAxis("Horizontal") < 0){
+            _rb.velocity = new Vector2(-5f, 0f) * speed * Time.deltaTime;
+
+        }else if(Input.GetAxis("Horizontal") > 0){
+            _rb.velocity = new Vector2(5f, 0f) * speed * Time.deltaTime;
+
+        }else if(Input.GetAxis("Vertical") < 0){
+            _rb.velocity = new Vector2(0f, -5f) * speed * Time.deltaTime;
+
+        }else if(Input.GetAxis("Vertical") > 0){
+            _rb.velocity = new Vector2(0f, 5f) * speed * Time.deltaTime;
+
+        }
+    }
+}
